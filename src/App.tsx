f7eebf0bdaa176b9487a2a74d0fdda42b7532809
Esam Mohamed Mousa <75634158+itcyanbu@users.noreply.github.com>
2026-01-14 22:28:05 +0300

    fix: enforce min-height zero on flex containers to enable scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
           activeModule={activeModule}
           onModuleChange={setActiveModule}
         />
-        <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+        <div className="flex-1 flex flex-col min-w-0 overflow-hidden min-h-0">
           <Header />
           {renderModule()}
         </div>
