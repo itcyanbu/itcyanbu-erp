59075ae266ff2c47f1a68f9f3c1e2c0d47afdf2e
Esam Mohamed Mousa <75634158+itcyanbu@users.noreply.github.com>
2026-01-14 22:20:00 +0300

    fix: constrain app layout wrapper to force child scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
           activeModule={activeModule}
           onModuleChange={setActiveModule}
         />
-        <div className="flex-1 flex flex-col min-w-0">
+        <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
           <Header />
           {renderModule()}
         </div>
